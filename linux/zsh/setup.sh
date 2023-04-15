fd846cf0164d0c7a285a5802be832f5e492497ee
feelinglight <kolpacc123@mail.ru>
2023-04-15 09:03:37 +0500

    Добавлена инструкция по настройке Kubuntu

---
+++ linux/zsh/setup.sh
@@ -0,0 +1,9 @@
+sudo apt install -y git zsh zoxide fzf bat
+
+# oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# sutosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+cp zshrc ~/.zshrc
