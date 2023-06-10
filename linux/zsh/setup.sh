e0a4947db2f1248a528970ccdc4b46b6cbd07167
feelinglight <kolpacc123@mail.ru>
2023-06-10 14:23:54 +0500

    Сделан неинтерактивный режим установки zsh

--- linux/zsh/setup.sh
+++ linux/zsh/setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-sudo apt install -y git zsh zoxide fzf bat
+sudo apt install -y git zsh zoxide fzf bat wget
 
 cur_dir=$(pwd)
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # oh-my-zsh
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" "" --unattended
+sudo chsh -s /usr/bin/zsh "$USER"
 
-# sutosuggestions
+# autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 cp "$script_dir/zshrc" ~/.zshrc
