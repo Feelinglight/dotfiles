281804a599a046f5b6ffcdbacc61abf24e9e110e
feelinglight <kolpacc123@mail.ru>
2023-06-10 14:37:58 +0500

    Добавлена правильная обработка установки zsh с sudo

--- linux/zsh/setup.sh
+++ linux/zsh/setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+if [ "$EUID" == 0 ]; then
+    real_user="$SUDO_USER"
+else
+    real_user="$USER"
+fi
+
 sudo apt install -y git zsh zoxide fzf bat wget
 
 cur_dir=$(pwd)
@@ -6,10 +13,9 @@ script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" "" --unattended
-sudo chsh -s /usr/bin/zsh "$USER"
+sudo chsh -s /usr/bin/zsh "$real_user"
 
 # autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 cp "$script_dir/zshrc" ~/.zshrc
-
