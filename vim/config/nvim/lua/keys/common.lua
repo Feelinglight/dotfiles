256cc3d655203f8fa60904ab219d41a830c217ac
feelinglight <kolpacc123@mail.ru>
2023-11-22 18:42:49 +0500

    Отключена блокировка стрелочек в vim

--- vim/config/nvim/lua/keys/common.lua
+++ vim/config/nvim/lua/keys/common.lua
@@ -1,11 +1,5 @@
 require('keys/alias')
 
--- Стрелочки откл. Использовать hjkl
-nmap('<up>', ':echoe "Use k"<CR>', {silent = false})
-nmap('<down>', ':echoe "Use j"<CR>', {silent = false})
-nmap('<left>', ':echoe "Use h"<CR>', {silent = false})
-nmap('<right>', ':echoe "Use l"<CR>', {silent = false})
-
 -- Назначает дополнительной клавишей для отключения режима Ctrl + K
 imap('<C-k>', '<Esc>')
 
