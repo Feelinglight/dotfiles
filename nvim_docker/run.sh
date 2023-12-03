7d442e9c74e16a597674f3eb322c963b8515043f
feelinglight <kolpacc123@mail.ru>
2023-12-03 15:18:35 +0500

    Обновлен nvim_docker

--- nvim_docker/run.sh
+++ nvim_docker/run.sh
@@ -1,9 +1,15 @@
+#!/bin/bash
+
+nvim_config_folder="nvim_config"
+
+mkdir "$nvim_config_folder"
+
 docker run \
   -it \
   --rm \
   --cap-add SYS_ADMIN \
   --device /dev/fuse \
-  -v ./nvim_config:/root/.config/nvim \
+  -v $(realpath "$nvim_config_folder"):/root/.config/nvim \
   --security-opt apparmor:unconfined \
   nvim
 
