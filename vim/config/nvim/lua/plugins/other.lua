d952252bad1b74d4aa8a6fe499a549e84e69c2f8
Nalimov Dmitry <kolpacc123@mail.ru>
2023-12-05 07:57:03 +0500

    Добавлен плагин vim-repeat

--- vim/config/nvim/lua/plugins/other.lua
+++ vim/config/nvim/lua/plugins/other.lua
@@ -3,6 +3,8 @@ return {
   { 'tpope/vim-sleuth' },
   -- Shortcats для работы с обрамляющими скобками, кавычками и т. д.
   { 'tpope/vim-surround' },
+  -- Делает возможным использования . с vim-surround
+  { 'tpope/vim-repeat' },
 
   -- Shortcats для быстрого комментирования строк
   { 
