150148a400d2642294e6a08885ef33da43009897
Nalimov Dmitry <kolpacc123@mail.ru>
2023-12-05 07:35:40 +0500

    Добавлены плагины vim

---
+++ vim/config/nvim/lua/plugins/other.lua
@@ -0,0 +1,17 @@
+return {
+  -- Автоматическое определение размера отступов в файле
+  { 'tpope/vim-sleuth' },
+  -- Shortcats для работы с обрамляющими скобками, кавычками и т. д.
+  { 'tpope/vim-surround' },
+
+  -- Shortcats для быстрого комментирования строк
+  { 
+    'numToStr/Comment.nvim',
+    lazy = false,
+    config = function()
+      require('Comment').setup()
+    end
+  },
+
+}
+
