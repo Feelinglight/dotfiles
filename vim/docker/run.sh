e2998a057bc715a335ec2fffd880a72a9dc46611
Nalimov Dmitry <kolpacc123@mail.ru>
2023-12-22 08:24:14 +0500

    Добавлено сохранение данных nvim между перезапусками докера

--- vim/docker/run.sh
+++ vim/docker/run.sh
@@ -5,6 +5,10 @@ set -e
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 nvim_config_folder="$script_dir/../config/nvim"
+# Чтобы не переустанавливать все плагины на перезапусках докера
+# Добавлена в .gitignore
+nvim_data_folder="$script_dir/data/nvim"
+mkdir -p "$nvim_data_folder"
 
 docker run \
   -it \
@@ -12,6 +16,7 @@ docker run \
   --cap-add SYS_ADMIN \
   --device /dev/fuse \
   -v $(realpath "$nvim_config_folder"):/root/.config/nvim \
+  -v $(realpath "$nvim_data_folder"):/root/.local/share/nvim \
   --security-opt apparmor:unconfined \
   nvim
 
