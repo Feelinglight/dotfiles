ce2ed04964a6503af1163384e08999deff659ca4
Nalimov Dmitry <kolpacc123@mail.ru>
2024-01-17 07:57:43 +0500

    Добавлен плагин which-key в vim

--- vim/config/nvim/lua/plugins/editor.lua
+++ vim/config/nvim/lua/plugins/editor.lua
@@ -1,5 +1,37 @@
 return {
-    -- Flash enhances the built-in search functionality by showing labels
+  -- which-key helps you remember key bindings by showing a popup
+  -- with the active keybindings of the command you started typing.
+  {
+    "folke/which-key.nvim",
+    lazy = false,
+    opts = {
+      plugins = { spelling = true },
+      defaults = {
+        mode = { "n", "v" },
+        ["g"] = { name = "+goto" },
+        ["gs"] = { name = "+surround" },
+        ["]"] = { name = "+next" },
+        ["["] = { name = "+prev" },
+        ["<leader><tab>"] = { name = "+tabs" },
+        ["<leader>b"] = { name = "+buffer" },
+        ["<leader>c"] = { name = "+code" },
+        ["<leader>f"] = { name = "+file/find" },
+        ["<leader>g"] = { name = "+git" },
+        ["<leader>gh"] = { name = "+hunks" },
+        ["<leader>q"] = { name = "+quit/session" },
+        ["<leader>s"] = { name = "+search" },
+        ["<leader>u"] = { name = "+ui" },
+        ["<leader>w"] = { name = "+windows" },
+        ["<leader>x"] = { name = "+diagnostics/quickfix" },
+      },
+    },
+    config = function(_, opts)
+      local wk = require("which-key")
+      wk.setup(opts)
+      wk.register(opts.defaults)
+    end,
+  },
+  -- Flash enhances the built-in search functionality by showing labels
   -- at the end of each match, letting you quickly jump to a specific
   -- location.
   {
@@ -9,6 +41,8 @@ return {
       modes = {
         -- fFtT
         char = {
+          -- Отключено, потому что плохо работает с операциями типа d
+          enabled = false,
           -- Отключить мультистроковый поиск
           multi_line = false,
           -- Показывать лэйблы на местах найденых символов
