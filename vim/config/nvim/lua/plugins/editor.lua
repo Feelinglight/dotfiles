b070dbe3e4ca10b44533bb31a8fa125df9ce7fbd
feelinglight <kolpacc123@mail.ru>
2024-06-14 22:15:54 +0500

    Добавлен плагин wilder

--- vim/config/nvim/lua/plugins/editor.lua
+++ vim/config/nvim/lua/plugins/editor.lua
@@ -475,5 +475,31 @@ return {
     },
   },
 
+  {
+    "gelguy/wilder.nvim",
+    config = function()
+      local wilder = require("wilder")
+      wilder.setup({
+        modes = { ":", "/", "?" },
+        next_key = "<C-j>",
+        previous_key = "<C-k>",
+        accept_key = "<Tab>",
+        -- enable_cmdline_enter = 0,
+      })
+
+      wilder.set_option(
+        "renderer",
+        wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+          highlights = {
+            border = "Normal", -- highlight to use for the border
+          },
+          -- 'single', 'double', 'rounded' or 'solid'
+          -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+          border = "rounded",
+        }))
+      )
+    end,
+  }
+
 }
 
