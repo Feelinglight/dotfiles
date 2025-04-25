007d5be7247663593f80581a51b4a154bcd08f02
feelinglight <kolpacc123@mail.ru>
2025-04-25 23:11:07 +0500

    Добавлены автокоманды из старого конфига

--- config/lazyvim/lua/config/autocmds.lua
+++ config/lazyvim/lua/config/autocmds.lua
@@ -15,3 +15,45 @@
 vim.api.nvim_create_autocmd("FileType", {
   command = "set formatoptions-=cro",
 })
+
+-- -- Открыть Neo-tree при запуске neovim
+-- vim.api.nvim_create_autocmd("VimEnter", {
+--   group = augroup("neotree", { clear = true }),
+--   desc = "Open Neotree on startup",
+--   callback = function()
+--     -- Если nvim открыт без аргументов, то будет открыт dashboard. Там neo-tree не показываем
+--     if vim.api.nvim_cmd({ cmd = "args" }, { output = true }) == "" then
+--       return
+--     end
+--     -- Без задержки валятся ошибки при открытии файлов
+--     vim.defer_fn(function()
+--       vim.cmd("Neotree show")
+--     end, 100)
+--   end,
+-- })
+
+-- Автоматическое переключение раскладки в нормальном режиме
+local function check_dbus()
+  local ret = os.execute("qdbus --version > /dev/null 2>&1")
+  if ret == 0 then
+    return true
+  else
+    return false
+  end
+end
+
+local has_dbus = check_dbus()
+-- Число зависит от настроек KDE, обычно 0
+local ENGLISH_LANGUAGE = 0
+
+local function set_current_language(language)
+  os.execute(string.format("qdbus org.kde.keyboard /Layouts setLayout %d > /dev/null", language))
+end
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  callback = function()
+    if has_dbus then
+      set_current_language(ENGLISH_LANGUAGE)
+    end
+  end,
+})
---
+++ config/lazyvim/queries/python/injections.scm
@@ -0,0 +1,27 @@
+;; extends
+
+;; Pydocs
+
+; function/method docstring
+; inject ReStructuredText
+
+; (function_definition
+;   body: (block .
+;     (expression_statement
+;       (string
+;         (string_content) @injection.content (#set! injection.language "rst")
+;       )
+;     )
+;   )
+; )
+;
+; (class_definition
+;   body: (block .
+;     (expression_statement
+;       (string
+;         (string_content) @injection.content (#set! injection.language "rst")
+;       )
+;     )
+;   )
+; )
+;
