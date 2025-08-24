138af0ba9e682b2af177d4b5342adcf38cf3a892
feelinglight <kolpacc123@mail.ru>
2025-08-24 14:28:49 +0500

    Добавлен hop.nvim. Выключен flash.nvim

---
+++ config/nvim/lua/plugins/editor/hop.lua
@@ -0,0 +1,10 @@
+return {
+  "smoka7/hop.nvim",
+  version = "*",
+  opts = {
+    keys = "jetovxqpdygfblzhckisuranmw",
+  },
+  keys = {
+    { "<leader>j", "<cmd>HopWord<cr>", desc = "Jump to word in current buffer" },
+  },
+}
